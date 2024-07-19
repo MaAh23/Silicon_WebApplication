ac9e021d66311ce26cb49f9099f8563d66b3e0af
Mahdi <mahdi.ahmadi7@outlook.com>
2024-07-19 13:50:31 +0200

    Add project files.

---
+++ WebApp/Controllers/CoursesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApp.ViewModels;
+
+
+
+namespace WebApp.Controllers;
+
+[Authorize]
+public class CoursesController(HttpClient httpClient) : Controller
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    [Route("/courses")]
+    public async Task<IActionResult> Index()
+    {
+        var viewModel = new CourseIndexViewModel();
+
+        var resposne = await _httpClient.GetAsync("https://localhost:7242/api/courses");
+        if (resposne.IsSuccessStatusCode)
+        {
+            var courses = JsonConvert.DeserializeObject<IEnumerable<CourseViewModel>>(await resposne.Content.ReadAsStringAsync());
+            if (courses != null && courses.Any())
+                viewModel.Courses = courses;
+            
+        }
+
+
+        return View(viewModel);
+    }
+}
---
+++ WebApp/ViewModels/CourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.ViewModels;
+
+public class CourseViewModel
+{
+    public int Id { get; set; }
+    public bool IsBestSeller { get; set; }
+    public string Image { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string Price { get; set; } = null!;
+    public string? DiscoundPrice { get; set; }
+    public string Hourse { get; set; } = null!;
+    public string LikeInProcent { get; set; } = null!;
+    public string LikeInNumbers { get; set; } = null!;
+}
